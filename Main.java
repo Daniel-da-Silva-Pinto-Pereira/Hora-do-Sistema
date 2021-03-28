759ddd2052ef28ff10da1a7f116a62ab68c94fe3
Daniel da Silva <danieldasilva@ucl.br>
2021-03-28 19:52:51 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class Main {
+  public static void main(String[] args) {
+    Date data = new Date();
+    System.out.println("A Hora do Sistema é");
+    System.out.println(data.toString());
+  }
+  }
+
+  
